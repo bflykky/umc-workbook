569e257a81adc75d5931396bc08261fb7d0dcc8f
bflykky <kkyxw@soongsil.ac.kr>
2024-06-26 21:49:28 +0900

    feat: 내 리뷰 목록 조회 API 구현

--- src/main/java/com/umc/week6/domain/review/service/ReviewService.java
+++ src/main/java/com/umc/week6/domain/review/service/ReviewService.java
@@ -1,13 +1,11 @@
 package com.umc.week6.domain.review.service;
 
 import com.umc.week6.domain.review.dto.ReviewRequest.AddReviewRequest;
-import com.umc.week6.domain.review.dto.ReviewResponse;
 import com.umc.week6.domain.review.dto.ReviewResponse.PagedReviewInfo;
 import com.umc.week6.domain.review.dto.ReviewResponse.ReviewId;
 
 public interface ReviewService {
-
     ReviewId addReview(AddReviewRequest request);
-
     PagedReviewInfo findReviewListByStoreId(Long storeId, Integer page);
+    PagedReviewInfo findMyReviewList(Long memberId, Integer page);
 }
--- src/main/java/com/umc/week6/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/umc/week6/domain/review/service/ReviewServiceImpl.java
@@ -4,7 +4,7 @@ import com.umc.week6.domain.member.entity.Member;
 import com.umc.week6.domain.member.service.MemberService;
 import com.umc.week6.domain.review.converter.ReviewConverter;
 import com.umc.week6.domain.review.dto.ReviewRequest;
-import com.umc.week6.domain.review.dto.ReviewResponse;
+import com.umc.week6.domain.review.dto.ReviewResponse.PagedReviewInfo;
 import com.umc.week6.domain.review.dto.ReviewResponse.ReviewId;
 import com.umc.week6.domain.review.entity.Review;
 import com.umc.week6.domain.review.repository.ReviewRepository;
@@ -13,6 +13,7 @@ import com.umc.week6.domain.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,11 +38,19 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public ReviewResponse.PagedReviewInfo findReviewListByStoreId(Long storeId, Integer page) {
+    public PagedReviewInfo findReviewListByStoreId(Long storeId, Integer page) {
         // 해당 storeId를 가진 Store가 존재하는지 검증
         storeService.getStore(storeId);
 
-        Page<Review> reviewList = reviewRepository.findByStoreId(storeId, PageRequest.of(page, 10));
+        Page<Review> reviewList = reviewRepository.findByStoreId(storeId, PageRequest.of(page, 10, Sort.Direction.DESC, "createdAt"));
+        return reviewConverter.toPagedReviewInfo(reviewList);
+    }
+
+    @Override
+    public PagedReviewInfo findMyReviewList(Long memberId, Integer page) {
+        Member member = memberService.getMember(memberId);
+
+        Page<Review> reviewList = reviewRepository.findByMemberId(member.getId(), PageRequest.of(page, 10, Sort.Direction.DESC, "createdAt"));
         return reviewConverter.toPagedReviewInfo(reviewList);
     }
 }
